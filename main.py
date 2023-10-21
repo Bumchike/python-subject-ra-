28ee14da18dc6617a55d968e24d8c31511181b8a
Bumchike <radukutov@gmail.com>
2023-10-21 13:07:36 +0200

    test is you are 18

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import time
 
 print("Введите ваш возраст")
-vozrast = input()
-minimum = int(18)
+vozrast = input(int)
 
-vozrast = int()
+vozrast = int(vozrast)
+minimum = int(18)
 
 if vozrast >= minimum:
     print("Отлично!")
     time.sleep(2)
     print("Переадресовка...")
-else: print("Вы не можете сделать банковскую карту если вам нет 18!")
+else:
+    print("Вы не можете сделать банковскую карту если вам нет 18!")
+    time.sleep(2)
+    print("Переадресовка...")
