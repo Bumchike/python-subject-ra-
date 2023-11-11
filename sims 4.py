b55782c611a581d83244801aec164e4cd4f48ad6
Bumchike <radukutov@gmail.com>
2023-11-11 12:14:09 +0100

    shitty sims 1 in console

---
+++ sims 4.py
@@ -0,0 +1,27 @@
+class human:
+    def __init__(self,name,work,car,home):
+        self.money = 10000
+        self.name = name
+        self.happiness = 60
+        self.nohunger = 80
+        self.work = work
+        self.car = car
+        self.home = home
+
+class car:
+    def __init__(self,brand,oil,strong,roshod):
+        self.brand = 0
+        self.oil = 0
+        self.strong = 0
+        self.roshod = 0
+
+class house:
+    def __init__(self,food,cleaness):
+        self.food = 0
+        self.cleaness = 0
+
+class job:
+    def __init__(self,job,list):
+        self.job = 0
+        self.gladness = 0
+        self.salary = 0
